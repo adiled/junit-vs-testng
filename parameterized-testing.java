67c58afac21ef6a812e6ff4e387fad741a7f5f0e
Muhammad Adil Shaikh <dareknyght@gmail.com>
2015-05-09 00:10:13 +0500

    Update parameterized-testing.java

--- parameterized-testing.java
+++ parameterized-testing.java
@@ -1,37 +1,32 @@
 
 
 /* ============================================
-== JUNIT PARAMETERIZED TESTING - Version 4.5+
+== JUnit PARAMETERIZED TESTING - Version 4.5+
 ===============================================*/
 
 
 @RunWith(Parameterized.class)
 public class FibonacciTest {
     
-    private Integer input;
-    private Integer expected;
+    private Integer input, expected;
 
     @Parameters
-    public static List data()
-    {
+    public static List data() {
         return Arrays.asList(new Integer[][] { { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 } });
     }
 
     @BeforeClass
-    public static void beforeClass()
-    {
+    public static void beforeClass() {
         System.out.println("Before")
     }
 
-    public FibonacciTest(Integer input, Integer expected)
-    {
+    public FibonacciTest(Integer input, Integer expected) {
         this.input = input;
         this.expected = expected;
     }
 
     @Test
-    public void FibonacciTest()
-    {
+    public void FibonacciTest() {
         System.out.println("Input: " + input + " Expected: " + expected);
         Assert.assertEquals(expected, Fibonacci.compute(input));
         assertEquals(expected, Fibonacci.compute(input));
@@ -44,16 +39,25 @@ public class FibonacciTest {
 == TestNG PARAMETERIZED TESTING - USING DATA PROVIDERS
 ====================================================== */
 
-/* Test cases to test {@link ILookupService}. */
+public class ParamTestWithDataProvider1 {
+   private PrimeNumberChecker primeNumberChecker;
+   
+   @BeforeMethod
+   public void initialize() {
+      primeNumberChecker = new PrimeNumberChecker();
+   }
+   
+   // Set the Data Provider
+   @DataProvider(name = "dpname")
+   public static Object[][] primeNumbers() {
+      return new Object[][] {{2, true}, {6, false}, {19, true}, {22, false}, {23, true}};
+   }
 
-public class LookupServiceTests extends BaseTestCase
-{
-
-    @Test(dataProvider = "LookupValueProvider", dataProviderClass = LookupValueDataProvider.class)
-    public void testGetAllLookupValues(String row, LookupValueRequest request, LookupValueResponse expectedResponse)
-    {
-        ILookupManager manager = super.getLookupManager();
-        LookupValueResponse actualResponse = manager.getLookupValues(request);
-        Assert.assertEquals(actualResponse.getStatus(), expectedResponse.getStatus());
-    }
+   // Use the Data Provider
+   @Test(dataProvider = "dpname")
+   public void testPrimeNumberChecker(Integer inputNumber, Boolean expectedResult) {
+      System.out.println(inputNumber + " " + expectedResult);
+      
+      Assert.assertEquals(expectedResult, primeNumberChecker.validate(inputNumber));
+   }
 }
