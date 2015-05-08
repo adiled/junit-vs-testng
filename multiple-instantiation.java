98945cd6b01bed0f97049838345f599354c9944c
Muhammad Adil Shaikh <dareknyght@gmail.com>
2015-05-08 18:28:33 +0500

    multiple-instantiation.java

---
+++ multiple-instantiation.java
@@ -0,0 +1,35 @@
+/**======= JUNIT - INSTANTIATION =======**/
+
+public class PainOneTest extends TestCase {
+
+  /* PainOneTest */
+  public PainOneTest() {
+    System.out.println("PainOneTest");
+  }
+  
+  /* @see TestCase#setUp() */
+  protected void setUp() throws Exception {
+    System.out.println("setUp()");
+  }
+  
+  /* @see TestCase#tearDown() */
+  protected void tearDown() throws Exception {
+    System.out.println("tearDown()");
+  }
+  
+  /* testMe */
+  public void testMe() {
+    System.out.println("testMe");
+  }
+  
+  /* testYou */
+  public void testYou() {
+    System.out.println("testYou");
+  }
+  
+}
+
+
+/**======= TESTNG - INSTANTIATION =======**/
+
+
